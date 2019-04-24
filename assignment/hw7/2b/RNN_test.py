ab55e62a2ecb2e44dc08fe594af0a55bf517e2d6
Taiga Hasegawa <37801134+TaigaHasegawa@users.noreply.github.com>
2019-04-23 22:07:25 -0500

    Add files via upload

---
+++ assignment/hw7/2b/RNN_test.py
@@ -0,0 +1,78 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import torch.distributed as dist
+
+import time
+import os
+import sys
+import io
+
+from RNN_model import RNN_model
+
+vocab_size = 8000
+model = torch.load('rnn_with_glove.model')
+model.cuda()
+
+batch_size = 200
+no_of_epochs = 10
+L_Y_train = len(y_train)
+L_Y_test = len(y_test)
+
+test_accu = []
+
+for epoch in range(no_of_epochs):
+
+    sequence_length = (epoch+1)*50
+
+    model.eval()
+
+    epoch_acc = 0.0
+    epoch_loss = 0.0
+
+    epoch_counter = 0
+
+    time1 = time.time()
+    
+    I_permutation = np.random.permutation(L_Y_test)
+
+    for i in range(0, L_Y_test, batch_size):
+
+        x_input2 = [x_test[j] for j in I_permutation[i:i+batch_size]]
+         
+        x_input = np.zeros((batch_size,sequence_length),dtype=np.int)
+        for j in range(batch_size):
+            x = np.asarray(x_input2[j])
+            sl = x.shape[0]
+            if(sl < sequence_length):
+                x_input[j,0:sl] = x
+            else:
+                start_index = np.random.randint(sl-sequence_length+1)
+                x_input[j,:] = x[start_index:(start_index+sequence_length)]
+        x_input = glove_embeddings[x_input]
+        y_input = y_train[I_permutation[i:i + batch_size]]
+        data = Variable(torch.FloatTensor(x_input)).cuda()
+        target = Variable(torch.FloatTensor(y_input)).cuda()
+
+        with torch.no_grad():
+            loss, pred = model(data,target)
+        
+        prediction = pred >= 0.0
+        truth = target >= 0.5
+        acc = prediction.eq(truth).sum().cpu().data.numpy()
+        epoch_acc += acc
+        epoch_loss += loss.data.item()
+        epoch_counter += batch_size
+
+    epoch_acc /= epoch_counter
+    epoch_loss /= (epoch_counter/batch_size)
+
+    test_accu.append(epoch_acc)
+
+    time2 = time.time()
+    time_elapsed = time2 - time1
+
+    print("%.1f" %sequence_length, "%.2f" % (epoch_acc*100.0), "%.4f" % epoch_loss)
